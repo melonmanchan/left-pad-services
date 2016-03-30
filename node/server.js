42d011e199d5381fa809a6b0b00765041863b603
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-30 10:34:07 +0300

    Add eslint

--- node/server.js
+++ node/server.js
@@ -4,7 +4,10 @@ import ip      from 'ip';
 const app  = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+    res.send('alrigth');
+});
+
 app.listen(port, () => {
     console.log(`Express app running at ${ip.address()}:${port}`);
-
 });
