f9dd11a3157120914d1fa7595a1a7854ac493dbc
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-30 14:29:45 +0300

    Add test for missing paddable char

--- node/test/index.js
+++ node/test/index.js
@@ -27,6 +27,18 @@ test('Should handle missing paddable string correctly', t => {
         });
 });
 
+test('Should handle missing char string', t => {
+    request(app)
+        .get('/?str=paddin%27%20oswalt&len=68')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+            if (err) throw err;
+            t.equal(res.body.str, `                                                      paddin' oswalt`)
+            t.end();
+        });
+});
+
 test('Should handle no parameters', t => {
     request(app)
         .get('/')
