9a5687ade73209f1262ac1a25a90c23f9c4fb5d9
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-30 17:01:12 +0300

    Add some failing tests

--- python/test.py
+++ python/test.py
@@ -17,5 +17,17 @@ class LeftPadTestCase(unittest.TestCase):
         output = self.get_json("/?str=paddin%27%20oswalt&len=68&ch=@")
         assert output["str"] == '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@paddin\' oswalt'
 
+    def test_should_handle_missing_paddable_string(self):
+        output = self.get_json("/?len=68&ch=@")
+        assert output["str"] == '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@'
+
+    def test_should_handle_missing_char_string(self):
+        output = self.get_json("/?str=paddin%27%20oswalt&len=68")
+        assert output["str"] == '                                                      paddin\' oswalt'
+
+    def test_should_handle_no_parameters(self):
+        output = self.get_json("")
+        assert output["str"] == ''
+
 if __name__ == "__main__":
     unittest.main()
