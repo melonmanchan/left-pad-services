86fbe9e25e982e003fd859d92d513b6ae9367c1e
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-30 11:16:34 +0300

    Add check for undefined paddable string

--- node/server.js
+++ node/server.js
@@ -6,11 +6,12 @@ import leftpad from './left-pad';
 const app  = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    const str = req.query.str;
+app.get('/', (req, res, next) => {
+    let str = req.query.str;
     const ch  = req.query.ch;
     const len = req.query.len;
 
+    typeof str === 'undefined' ? str = '' : str = str;
     res.status(200).json({ str: leftpad(str, len, ch) });
 });
 
