7ed5e8214599af6718557d8c64d2333482e99df3
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-30 16:19:04 +0300

    Add functional pad as a service

--- python/server.py
+++ python/server.py
@@ -8,8 +8,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return jsonify(str="hallo")
+    str = request.args.get("str") or ""
+    ch  = request.args.get("ch")  or " "
+    len = request.args.get("len") or 0
+
+    return jsonify(str=str.rjust(int(len), ch))
 
 if __name__ == "__main__":
-    app.run(port=3000)
+    app.run(port=3000, debug=True)
 
