c63b5f0f930c1c4f5155bb5f4339a9c98949ef2e
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-30 16:54:59 +0300

    More DRY

--- python/test.py
+++ python/test.py
@@ -2,12 +2,20 @@
 
 import unittest
 import server
+import json
 
 class LeftPadTestCase(unittest.TestCase):
 
     def setUp(self):
         server.app.config["TESTING"] = True
         self.client = server.app.test_client()
+
+    def get_json(self, url):
+       return json.loads(self.client.get(url).data)
+
+    def test_should_pad_string(self):
+        output = self.get_json("/?str=paddin%27%20oswalt&len=68&ch=@")
+        assert output["str"] == '@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@paddin\' oswalt'
 
 if __name__ == "__main__":
     unittest.main()
