7202490732913cbc4ddb67fbf402496c980f2184
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-30 12:27:23 +0300

    Add some null variable checks

--- ruby/server.rb
+++ ruby/server.rb
@@ -7,8 +7,8 @@ require_relative 'left-pad'
 
 get '/' do
     str = params['str'] || ""
-    ch  = params['ch']
-    len = params['len']
+    ch  = params['ch']  || ""
+    len = params['len'] || 0
 
     { str: LeftPad.leftPad(str, len, ch) }.to_json
 end
