b15c153e2b53fbf1b538aac482600939ee40ffab
Matti Jokitulppo <melonmanchan@gmail.com>
2016-03-31 12:42:41 +0300

    Add JSON response test

--- clojure/src/leftpad/handler.clj
+++ clojure/src/leftpad/handler.clj
@@ -1,11 +1,13 @@
 (ns leftpad.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (response {:foo "bar"}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-json-response app-routes site-defaults))
