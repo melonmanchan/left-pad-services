61900c81318f98b0fd45a04f8a3be1f56cc8b3ca
Matti Jokitulppo <melonmanchan@gmail.com>
2016-04-02 19:54:20 +0300

    Fix some types, better way of interpolating values

--- dart/server.dart
+++ dart/server.dart
@@ -2,16 +2,16 @@ import 'dart:io';
 import 'dart:convert' show JSON;
 
 main() {
-    var port = 3000;
+    int port = 3000;
 
     HttpServer.bind('localhost', port).then((HttpServer server) {
-        print('Server started on port: ${port}');
+        print('Server started on port: $port');
         server.listen((HttpRequest request) {
                 String str = request.uri.queryParameters['str'] ?? '';
                 String ch = request.uri.queryParameters['ch'] ?? ' ';
                 String len = request.uri.queryParameters['len'] ?? '0';
 
-                Object output = {'str': str.padLeft(int.parse(len), ch)};
+                Map output = {'str': str.padLeft(int.parse(len), ch)};
 
                 request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json');
                 request.response..headers.set('Access-Control-Allow-Origin','*');
