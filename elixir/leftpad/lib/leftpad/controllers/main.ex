fc8b0786019d15227f349a24e9aebe3b8a598198
Matti Jokitulppo <melonmanchan@gmail.com>
2016-04-08 23:25:22 +0300

    Baby steps in elixir project

--- elixir/leftpad/lib/leftpad/controllers/main.ex
+++ elixir/leftpad/lib/leftpad/controllers/main.ex
@@ -2,10 +2,12 @@ defmodule Leftpad.Controllers.Main do
   use Sugar.Controller
 
   def index(conn, []) do
-      str = conn.params[:str] || ""
-      ch  = conn.params[:chk] || ""
-      len = conn.params[:len] || 0
+      str = conn.params["str"] || ""
+      ch  = conn.params["ch"] || ""
+      len = conn.params["len"] || 0
 
-      json conn, %{str: String.rjust(str, len, ch)}
+      padded = String.rjust(str, String.to_integer(len), ?a)
+
+      json conn, %{str: padded}
   end
 end
