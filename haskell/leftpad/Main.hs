a025f77e85da95d9cd87e833407d219e7fe5e848
Matti Jokitulppo <melonmanchan@gmail.com>
2016-04-17 17:22:06 +0300

    Add functional left pad

--- haskell/leftpad/Main.hs
+++ haskell/leftpad/Main.hs
@@ -5,12 +5,22 @@ import GHC.Generics
 import Web.Scotty
 import Data.Aeson (ToJSON)
 
-data StrOutput = StrOutput { str :: String } deriving (Generic)
+data PadOutput = PadOutput { str :: String } deriving (Generic)
+instance ToJSON PadOutput
 
-instance ToJSON StrOutput
+-- Snippet originally from https://hackage.haskell.org/package/acme-left-pad
+leftPad :: String -> Int -> String -> String
+leftPad s l "" = leftPad' s l ' '
+leftPad s l (c:[]) = leftPad' s l c
+
+leftPad' s 0 c = s
+leftPad' s n c = if length s < n then [c] ++ leftPad' s (n - 1) c else leftPad' s (n - 1) c
 
 main :: IO ()
 main = scotty 3000 $ do
   get "/" $ do
-    let out = StrOutput { str = "test" }
+    stringToPad <- param "str"
+    padding     <- param "ch"
+    padLength   <- param "len"
+    let out = PadOutput { str = leftPad stringToPad padLength padding}
     json out
