a98221272f43a754b6f8ffddef7d27ab41099c3d
Matti Jokitulppo <melonmanchan@gmail.com>
2016-04-30 21:31:38 +0300

    Add working REST API with Iron

---
+++ rust/leftpad/README.md
@@ -0,0 +1,3 @@
+# Rust
+
+Made with Rust lang and Iron, use cargo run to start server
--- rust/leftpad/src/main.rs
+++ rust/leftpad/src/main.rs
@@ -1,3 +1,24 @@
+extern crate iron;
+extern crate rustc_serialize;
+use rustc_serialize::json;
+
+use iron::prelude::*;
+use iron::status;
+
+
+#[derive(RustcEncodable)]
+pub struct PadJson {
+    str: String
+}
+
 fn main() {
-    println!("Hello, world!");
+    fn handler(_: &mut Request) -> IronResult<Response> {
+        let padded_resp = PadJson {
+            str: "Hallo".to_string(),
+        };
+
+        Ok(Response::with((status::Ok, json::encode(&padded_resp).unwrap())))
+    }
+
+    Iron::new(handler).http("localhost:3000").unwrap();
 }
