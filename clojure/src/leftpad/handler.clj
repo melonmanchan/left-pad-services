dfca90c5e210e0914ab96b1547a96f0a70591e8d
Matti Jokitulppo <melonmanchan@gmail.com>
2016-05-05 22:16:45 +0300

    Add working query parameter reading

--- clojure/src/leftpad/handler.clj
+++ clojure/src/leftpad/handler.clj
@@ -1,13 +1,17 @@
 (ns leftpad.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
+            [compojure.handler :as handler]
             [ring.middleware.json :refer [wrap-json-response]]
+            [ring.middleware.params :refer [wrap-params]]
             [ring.util.response :refer [response]]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (defroutes app-routes
-  (GET "/" [] (response {:foo "bar"}))
+  (GET "/" {:keys [params] :as request} (response {:foo (get params :str "none")}))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-json-response app-routes site-defaults))
+  (-> (handler/api app-routes)
+             (wrap-params)
+             (wrap-json-response)))
