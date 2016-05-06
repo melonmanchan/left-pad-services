615516e8e522477e9326105fe786270dcd920659
Matti Jokitulppo <melonmanchan@gmail.com>
2016-05-06 15:21:12 +0300

    Add GSON and test string output

--- java/src/main/java/Main.java
+++ java/src/main/java/Main.java
@@ -1,8 +1,16 @@
 import static spark.Spark.*;
+import com.google.gson.Gson;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         port(3000);
-        get("/", (req, res) -> "Hello world");
+        get("/", (req, res) -> {
+            res.type("application/json");
+            Map resp = new HashMap();
+            resp.put("str", "hello");
+            return new Gson().toJson(resp);
+        });
     }
 }
