cf58c5c680c4286407c6acfe23f49a4a6fb9e09f
Matti Jokitulppo <melonmanchan@gmail.com>
2016-05-14 18:04:39 +0300

    Start work on rust leftpad function

--- rust/leftpad/src/main.rs
+++ rust/leftpad/src/main.rs
@@ -12,20 +12,26 @@ pub struct PadJson {
     str: String
 }
 
+fn left_pad(string: &str, len: u32, ch: &str) -> String {
+    let i = -1;
+    let mut temp_len = len - (string.len() as u32);
+    return String::from("hallo");
+}
+
 fn main() {
     fn handler(req: &mut Request) -> IronResult<Response> {
 
         let query = req.url.query.clone().unwrap_or("".to_string());
 
         let query_params: Json = parse(&query.to_string()).unwrap();
-        let obj = query_params.as_object().unwrap();
+        let obj                = query_params.as_object().unwrap();
 
-        let str : &str = obj.get("str").unwrap().as_string().unwrap();
-        let len : u32 = obj.get("len").unwrap().as_string().unwrap().parse().unwrap();
-        let ch : &str = obj.get("ch").unwrap().as_string().unwrap();
+        let string : &str = obj.get("str").unwrap().as_string().unwrap();
+        let len : u32     = obj.get("len").unwrap().as_string().unwrap().parse().unwrap();
+        let ch : &str     = obj.get("ch").unwrap().as_string().unwrap();
 
         let padded_resp = PadJson {
-            str: String::from(str),
+            str: left_pad(string, len, ch),
         };
 
         Ok(Response::with((status::Ok, json::encode(&padded_resp).unwrap())))
